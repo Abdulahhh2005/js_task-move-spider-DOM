721cad2ef6514ada27f0b62ac0e66d4887f8b2ed
Ex1stent <142783655+Abdulahhh2005@users.noreply.github.com>
2025-09-29 13:10:46 +0300

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,35 @@
 
 // -----------------------------------------------------------------------------
 
+document.addEventListener('click', (e) => {
+  const spider = document.querySelector('.spider');
+  // Щоб курсор спрацьовував тілки на стіні
+  const wall = e.target.closest('.wall');
+
+  if (!wall) {
+    return;
+  }
+
+  const spiderRect = spider.getBoundingClientRect();
+  const wallRect = wall.getBoundingClientRect();
+
+  // Координати кліка всередині стіни, центр павука під курсором
+  let x = e.clientX - wallRect.left - spiderRect.width / 2;
+  let y = e.clientY - wallRect.top - spiderRect.height / 2;
+
+  // Обмеження руху павука всередині стіни (використовуємо clientWidth/Height)
+  x = Math.max(0, Math.min(x, wallRect.width - spiderRect.width));
+  y = Math.max(0, Math.min(y, wallRect.height - spiderRect.height));
+
+  spider.style.left = `${x}px`;
+  spider.style.top = y + 'px';
+});
+
+// -----------------------------------------------------------------------------
+
+// eslint-disable-next-line max-len
+// Різниця від першого варіанту в тому, що клік спрацьовує лише всередині стіни, павук тягнеться до курсору, але не виходить за рамки.
+
 // document.addEventListener('click', (e) => {
 //   const wall = document.querySelector('.wall');
 //   const spider = document.querySelector('.spider');
@@ -32,34 +61,5 @@
 //   spider.style.top = `${newTop}px`;
 //   spider.style.left = `${newLeft}px`;
 // });
-
-// -----------------------------------------------------------------------------
-
-// eslint-disable-next-line max-len
-// Різниця від першого варіанту в тому, що клік спрацьовує лише всередині стіни, павук тягнеться до курсору, але не виходить за рамки.
-
-document.addEventListener('click', (e) => {
-  const spider = document.querySelector('.spider');
-  // Щоб курсор спрацьовував тілки на стіні
-  const wall = e.target.closest('.wall');
-
-  if (!wall) {
-    return;
-  }
-
-  const spiderRect = spider.getBoundingClientRect();
-  const wallRect = wall.getBoundingClientRect();
-
-  // Координати кліка всередині стіни, центр павука під курсором
-  let x = e.clientX - wallRect.left - spiderRect.width / 2;
-  let y = e.clientY - wallRect.top - spiderRect.height / 2;
-
-  // Обмеження руху павука всередині стіни (використовуємо clientWidth/Height)
-  x = Math.max(0, Math.min(x, wallRect.width - spiderRect.width));
-  y = Math.max(0, Math.min(y, wallRect.height - spiderRect.height));
-
-  spider.style.left = `${x}px`;
-  spider.style.top = y + 'px';
-});
 
 // -----------------------------------------------------------------------------
